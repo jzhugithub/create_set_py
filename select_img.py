bb4609177fb7c812c0cd530057f45885c92affa1
jzhugithub <mfx106825372@163.com>
2018-01-28 10:44:49 +0800

    add auto mode

--- select_img.py
+++ select_img.py
@@ -9,15 +9,16 @@ import cv2
 import os
 
 
-def run(video_path, out_folder_path, label, frame_interval):
+def run(video_path, out_folder_path, label, frame_interval, auto_mode):
     img_count = 1
+    tmp_char = 'n'
     my_video = cv2.VideoCapture(video_path)
 
     if not my_video.isOpened():
         print('video open error')
         return
     else:
-        cv2.namedWindow("img")
+        cv2.namedWindow("img", cv2.WINDOW_NORMAL)
         while True:
             success, src = my_video.read()
             if not success:
@@ -26,7 +27,19 @@ def run(video_path, out_folder_path, label, frame_interval):
 
             stop = False
             while (not stop):
-                n = chr(cv2.waitKey(0))
+
+                if auto_mode:
+                    cv2.waitKey(1)
+
+                    if tmp_char == 'n':
+                        n = 's'
+                        tmp_char = 's'
+                    else:
+                        n = 'n'
+                        tmp_char = 'n'
+                else:
+                    n = chr(cv2.waitKey(0))
+
                 if n == 'n' or n == 'N':
                     for i in range(0, int(frame_interval)):
                         if not my_video.read()[0]:
@@ -46,9 +59,9 @@ if __name__ == '__main__':
     print('s: select this frame')
     print('other letter: exit')
 
-    video_path = '/home/zj/database/fisheye_data/video/out_fisheye01191.avi'
+    video_path = '/home/zj/database/fisheye_data/video/out_fisheye01194.avi'
     out_folder_path = '/home/zj/my_workspace/create_set_py/image_select'
-    label = 'out_fisheye01191_'
-    frame_interval = 5
+    label = 'out_fisheye01194_'
+    frame_interval = 3
 
-    run(video_path, out_folder_path, label, frame_interval)
+    run(video_path, out_folder_path, label, frame_interval, True)
